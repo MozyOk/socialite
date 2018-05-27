0e8cfdcab8671d5fa510f930615de898a88104e2
MozyOk <mozy.okubo@gmail.com>
2018-05-28 00:28:06 +0900

    make: user find or create function

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Socialite;
+use Auth;
+use App\SocialAccount;
+use App\User;
 
 class SocialAccountController extends Controller
 {
@@ -24,6 +28,32 @@ class SocialAccountController extends Controller
 
         Auth::login($authUser, true);
 
-        return redirect($this->redirectTo);
+        return redirect('/home');
+    }
+
+    public function findOrCreateUser($socialUser, $provider)
+    {
+        $account = SocialAccount::where('provider_name', $provider)->where('provider_id',
+        $socialUser->getID())->fisrt();
+
+        if($account){
+            return $account->user;
+        } else {
+            $user = User::where('email', $socialUser->getEmail())->first();
+
+            if (! $user) {
+                $user = User::create([
+                    'email' => $socialUser->getEmail(),
+                    'name' => $socialUser->getNmae(),
+                ]);
+            }
+
+            $user->accounts()->create([
+                'provider_name' => $provider,
+                'provider_id' => $socialUser->getId(),
+            ]);
+
+            return $user;
+        }
     }
 }
