949a0d3ad3f7347e966e169b4e90f10a2eafb99f
MozyOk <mozy.okubo@gmail.com>
2018-05-27 18:30:14 +0900

    make: account function

--- app/SocialAccount.php
+++ app/SocialAccount.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialAccount extends Model
 {
-    //
+    protected $fillable = [
+        'provider_name', 'provider_id'
+    ];
+    
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
